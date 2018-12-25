34a4bd19c88dec1d1280c24b5c6c633b701d114a
Satorino01 <satorino0821@gmail.com>
2018-12-26 03:29:50 +0900

    スプラッシュ画面の追加

--- app/src/main/java/com/example/kobayashi_satoru/miroyo/SendMovieActivity.java
+++ app/src/main/java/com/example/kobayashi_satoru/miroyo/SendMovieActivity.java
@@ -10,7 +10,16 @@ public class SendMovieActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // ここで1秒間スリープし、スプラッシュを表示させたままにする。
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);// スプラッシュthemeを通常themeに変更する
         setContentView(R.layout.send_movie_activity);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, SendMovieFragment.newInstance()).commitNow();
         }
