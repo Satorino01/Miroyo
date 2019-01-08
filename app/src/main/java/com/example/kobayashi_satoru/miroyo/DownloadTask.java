0d24fc937fe97dc57abc573a22392e44ef7bec7c
Satorino01 <satorino0821@gmail.com>
2019-01-09 08:49:32 +0900

    Facebookでのログイン機能の実装

---
+++ app/src/main/java/com/example/kobayashi_satoru/miroyo/DownloadTask.java
@@ -0,0 +1,38 @@
+package com.example.kobayashi_satoru.miroyo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class SquareImageGetTask extends AsyncTask<String,Void,Bitmap> {
+    private SquareImageView image;
+
+    public SquareImageGetTask(SquareImageView _image) {
+        image = _image;
+    }
+    @Override
+    protected Bitmap doInBackground(String... params) {
+        Bitmap image;
+        try {
+            URL imageUrl = new URL(params[0]);
+            InputStream imageIs;
+            imageIs = imageUrl.openStream();
+            image = BitmapFactory.decodeStream(imageIs);
+            return image;
+        } catch (MalformedURLException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        // 取得した画像をImageViewに設定します。
+        image.setImageBitmap(result);
+    }
+}
